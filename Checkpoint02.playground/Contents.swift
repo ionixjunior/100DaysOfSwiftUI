6ddbb42d31100a1264e6012178cfc40996e95cd8
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-04 18:28:19 -0300

    Add checkpoint 2

---
+++ Checkpoint02.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var names = ["Josh", "Carter", "Lucie", "Marie", "John", "Josh", "Junior", "Louis", "Beth", "Lucia", "David", "Marie", "Veronica", "Josh", "Veronica"]
+var uniqueNames = Set(names)
+
+print("The number of names is \(names.count)")
+print("The number of unique names is \(uniqueNames.count)")
