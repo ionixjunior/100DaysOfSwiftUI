9fdd2041b0d126a068c3df2b4e6d53b176c556bf
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-16 19:12:44 -0300

    Create WeSplit project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ione Souza Junior on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
