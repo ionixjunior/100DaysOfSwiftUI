ded83c362a6dd96d3c25c50945d729c4ad760cf1
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-18 17:43:56 -0300

    Add header title

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -48,7 +48,7 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 }
 
-                Section {
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
