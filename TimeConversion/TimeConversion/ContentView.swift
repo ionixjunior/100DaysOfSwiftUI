e848ae4a5a77e3cba9325cbd1b0439f2560f1b99
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-19 19:29:50 -0300

    Add pickers on screen

--- TimeConversion/TimeConversion/ContentView.swift
+++ TimeConversion/TimeConversion/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var unitFrom = Unit.seconds
+    @State private var unitTo = Unit.minutes
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Choose the unit from", selection: $unitFrom) {
+                        ForEach(Unit.allCases) { unit in
+                            Text(unit.rawValue.capitalized)
+                                .tag(unit)
+                        }
+                    }
+
+                    Picker("Choose the unit to", selection: $unitTo) {
+                        ForEach(Unit.allCases) { unit in
+                            Text(unit.rawValue.capitalized)
+                                .tag(unit)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Time Conversion")
         }
-        .padding()
     }
 }
 
