88cac5fa8963af3c17d33c8754329977456b94e6
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-19 18:59:47 -0300

    Implement convertion between seconds and minutes

--- TimeConversion/TimeConversion/Converter/Converter.swift
+++ TimeConversion/TimeConversion/Converter/Converter.swift
@@ -11,7 +11,19 @@ class Converter {
     private init() {}
 
     static func convert(value: Int, from: Unit, to: Unit) -> Int {
+        if (from == .seconds || from == .minutes) && (to == .seconds || to == .minutes) {
+            return convertBetweenSecondsAndMinutes(value: value, from: from, to: to)
+        }
+
         return 0
+    }
+
+    private static func convertBetweenSecondsAndMinutes(value: Int, from: Unit, to: Unit) -> Int {
+        if from == .seconds {
+            return value / 60
+        }
+
+        return value * 60
     }
 }
 
