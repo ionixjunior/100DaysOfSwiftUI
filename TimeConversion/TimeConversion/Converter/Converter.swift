bda310f3a276a8270c391219b3950e8a45dc3973
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-19 18:33:25 -0300

    Create Converter class

---
+++ TimeConversion/TimeConversion/Converter/Converter.swift
@@ -0,0 +1,13 @@
+//
+//  Converter.swift
+//  TimeConversion
+//
+//  Created by Ione Souza Junior on 19/07/24.
+//
+
+import Foundation
+
+class Converter {
+    private init() {}
+}
+
---
+++ TimeConversion/TimeConversionTests/Converter/ConverterTests.swift
@@ -0,0 +1,58 @@
+//
+//  ConverterTests.swift
+//  TimeConversionTests
+//
+//  Created by Ione Souza Junior on 19/07/24.
+//
+
+import XCTest
+
+final class ConverterTests: XCTestCase {
+    func testConverterSecondsToMinutes() {
+
+    }
+
+    func testConverterSecondsToHours() {
+
+    }
+
+    func testConverterSecondsToDays() {
+
+    }
+
+    func testConverterMinutesToSeconds() {
+
+    }
+
+    func testConverterMinutesToHours() {
+
+    }
+
+    func testConvertMinutesToDays() {
+
+    }
+
+    func testConvertHoursToSeconds() {
+
+    }
+
+    func testConvertHoursToMinutes() {
+
+    }
+
+    func testConvertHoursToDays() {
+
+    }
+
+    func testConvertDaysToSeconds() {
+
+    }
+
+    func testConvertDaysToMinutes() {
+
+    }
+
+    func testConvertDaysToHours() {
+
+    }
+}
