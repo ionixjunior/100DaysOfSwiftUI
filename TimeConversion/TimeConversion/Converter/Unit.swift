b81f45d6ffcdfd837bf83e94b30548509ca45f24
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-19 19:23:55 -0300

    Implement some protocols on Unit enum
    This will make possible to put it in a foreach and interate each option.

--- TimeConversion/TimeConversion/Converter/Unit.swift
+++ TimeConversion/TimeConversion/Converter/Unit.swift
@@ -5,6 +5,10 @@
 //  Created by Ione Souza Junior on 19/07/24.
 //
 
-enum Unit {
+enum Unit: String, Identifiable, CaseIterable {
     case seconds, minutes, hours, days
+
+    var id: String {
+        self.rawValue
+    }
 }
