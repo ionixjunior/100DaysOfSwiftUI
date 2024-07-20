6dd9ba489d39e9ebfb925bc65469a911547b3c16
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-20 14:37:35 -0300

    Create a simple game to guess the flag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var flags = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State var showAnswerAlert = false
+    @State var alertTitle = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.red, .blue], startPoint: .bottomLeading, endPoint: .topTrailing)
+
+            VStack {
+                Text("Select the flag of")
+                Text(flags[correctAnswer])
+
+                VStack(spacing: 20) {
+                    ForEach(0..<3) { flag in
+                        Button {
+                            showAnswer(flag)
+                        } label: {
+                            Image(flags[flag])
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+        .alert(alertTitle, isPresented: $showAnswerAlert, actions: {
+            Button("Next") {
+                showNextQuestion()
+            }
+        })
+    }
+
+    private func showAnswer(_ selectedFlag: Int) {
+        if correctAnswer == selectedFlag {
+            alertTitle = "Correct answer"
+        } else {
+            alertTitle = "Wrong answer"
+        }
+
+        showAnswerAlert = true
+    }
+
+    private func showNextQuestion() {
+        flags.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
