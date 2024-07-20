1911e6f17b81254f535d046cabcf877ac848325c
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-20 12:48:10 -0300

    Create the guess the flag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ione Souza Junior on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
