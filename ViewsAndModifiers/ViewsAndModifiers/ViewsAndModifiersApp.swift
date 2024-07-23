5790a57be43d20bad35ca5fb0db8ce8d2e178e98
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-23 18:19:27 -0300

    Create the project ViewAndModifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Ione Souza Junior on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
