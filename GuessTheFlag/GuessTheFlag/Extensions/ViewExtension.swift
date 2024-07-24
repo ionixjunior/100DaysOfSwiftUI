e83eeda8fbdcd552fad7a998a426953ebc7b9d22
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-24 20:50:25 -0300

    Create view extension

---
+++ GuessTheFlag/GuessTheFlag/Extensions/ViewExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ViewExtension.swift
+//  GuessTheFlag
+//
+//  Created by Ione Souza Junior on 24/07/24.
+//
+
+import SwiftUI
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
