cddc63bb11cedd2606470701dff3368b2442ff54
Ione Souza Junior <junior@ionixjunior.com.br>
2024-07-25 18:02:52 -0300

    Create WinOrLose project

---
+++ WinOrLose/WinOrLose/WinOrLoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinOrLoseApp.swift
+//  WinOrLose
+//
+//  Created by Ione Souza Junior on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WinOrLoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
